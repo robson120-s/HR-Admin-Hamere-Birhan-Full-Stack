5d24e66dc1f2aec5639de48a6ff4e24d0f140ee8
robson120-s <shroba391@gmail.com>
2025-09-16 22:40:18 +0300

    some requirement

--- HR_backend/server.js
+++ HR_backend/server.js
@@ -2,15 +2,6 @@ const express = require('express')
 const app = require('./app')
 const PORT = process.env.PORT || 5555;
 
-
-
-
-
-
-
-
-
-
 app.listen(PORT, ()=>{
     console.log(`Server is Listening on port ${PORT}`)
 })
