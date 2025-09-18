f97381b12ff52768d60f11e7de61f7beb92d4e12
robson120-s <shroba391@gmail.com>
2025-09-18 21:58:23 +0300

    attendance change

--- HR_backend/app/utils/attendanceProcessor.js
+++ HR_backend/app/utils/attendanceProcessor.js
@@ -1,5 +1,5 @@
 // app/utils/attendanceProcessor.js
-const { PrismaClient } = require('../generated/prisma');
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 const processAttendanceForDate = async (date, employeeIds) => {
